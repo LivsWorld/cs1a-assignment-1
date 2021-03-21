740c421f7ee93fbf604c753f6e9069320f569bf2
olivia shen <oliviashen1@icloud.com>
2021-03-21 15:40:06 -0700

    original commit to test out how git works

---
+++ Foothill.java
@@ -0,0 +1,52 @@
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("My name is Olivia Shen");
+      System.out.println("My student ID is 20442708");
+      
+      System.out.println();
+      System.out.println("Syllabus Detail #1: Students of CS 1A should expect");
+      System.out.println("to commit 10-14 hours per week on class work.");
+      
+      System.out.println();
+      System.out.println("Syllabus Detail #2: Office hours are held weekly");
+      System.out.println("on Thursdays from 2-3 PM using the Conferences tab");
+      System.out.println("in Canvas.");
+      
+      System.out.println();
+      System.out.println("Syllabus Detail #3: We will be using an IDE called");
+      System.out.println("Eclipse to run our Java programs.");
+      
+      System.out.println();
+      System.out.println("Style Rule #1: Do not write lines of code that");
+      System.out.println("exceed 80 characters.");
+      
+      System.out.println();
+      System.out.println("Style Rule #2: Never use tabs in your code!");
+   }
+}
+
+/* -------------------------------- OUTPUT ------------------------------------
+
+Hello World!
+My name is Olivia Shen
+My student ID is 20442708
+
+Syllabus Detail #1: Students of CS 1A should expect
+to commit 10-14 hours per week on class work.
+
+Syllabus Detail #2: Office hours are held weekly
+on Thursdays from 2-3 PM using the Conferences tab
+in Canvas.
+
+Syllabus Detail #3: We will be using an IDE called
+Eclipse to run our Java programs.
+
+Style Rule #1: Do not write lines of code that
+exceed 80 characters.
+
+Style Rule #2: Never use tabs in your code!
+
+---------------------------------------------------------------------------- */
